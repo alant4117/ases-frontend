41599609f595947cd271288bb24d2e80eca29330
alant4117 <alant4117@gmail.com>
2025-11-07 18:51:12 -0300

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'ases-green': '#064e3b',
+        'ases-gold': '#eab308',
+      },
+    },
+  },
+  plugins: [],
+}
